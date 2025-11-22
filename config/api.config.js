6435db08d3915e5e6078eb4e68a852c8683c2110
colla06 <132714511+colla06@users.noreply.github.com>
2025-11-22 22:02:32 +0100

    Change obfuscatedClientSecret value in api.config.js
    Updated the obfuscated client secret in api.config.js.

--- config/api.config.js
+++ config/api.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  clientId: 'c91885c5-a194-40f7-ac67-14524e73a35d', // tu app de Azure
-  obfuscatedClientSecret: 'xBb8Q~qoZ67rrW2fla0aPGW.2Fxba-IIbm-.5aCp', // el valor real del secret
+  clientId: 'c91885c5-a194-40f7-ac67-14524e73a35d', 
+  obfuscatedClientSecret: 'AQUI_PONES_EL_VALUE_DEL_SECRET', // copiar exactamente
   redirectUri: 'http://localhost',
   authApi: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
   driveApi: 'https://graph.microsoft.com/v1.0/me/drive',
