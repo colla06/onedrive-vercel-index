7c1abd05e639733e6d85c68389379bf6310ce3a5
colla06 <132714511+colla06@users.noreply.github.com>
2025-11-22 21:07:30 +0100

    Create multi-config.js

---
+++ config/multi-config.js
@@ -0,0 +1,23 @@
+/**
+ * Multi-account configuration for OVI
+ * Define las rutas y las cuentas de OneDrive
+ */
+module.exports = {
+  roots: [
+    {
+      id: 'HDL',
+      userPrincipalName: 'colla@3lwrsm.onmicrosoft.com',
+      baseDirectory: '/',
+    },
+    {
+      id: 'FHD',
+      userPrincipalName: 'JohannaL@3lwrsm.onmicrosoft.com',
+      baseDirectory: '/',
+    },
+    {
+      id: 'ONLINE',
+      userPrincipalName: 'HenriettaM@3lwrsm.onmicrosoft.com',
+      baseDirectory: '/',
+    },
+  ]
+}
