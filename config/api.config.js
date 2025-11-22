445165b261132f5aa1900c431b3faf998ce3001b
colla06 <132714511+colla06@users.noreply.github.com>
2025-11-22 21:59:58 +0100

    Update api.config.js

--- config/api.config.js
+++ config/api.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  clientId: 'c91885c5-a194-40f7-ac67-14524e73a35d',
-  obfuscatedClientSecret: 'U2FsdGVkX1830zo3/pFDqaBCVBb37iLw3WnBDWGF9GIB2f4apzv0roemp8Y+iIxI3Ih5ecyukqELQEGzZlYiWg==',
+  clientId: 'c91885c5-a194-40f7-ac67-14524e73a35d', // Id. de aplicación (cliente)
+  obfuscatedClientSecret: 'aquí_va_el_valor_real_del_secret', // el valor que generaste en Azure, no el ID
   redirectUri: 'http://localhost',
   authApi: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
   driveApi: 'https://graph.microsoft.com/v1.0/me/drive',
